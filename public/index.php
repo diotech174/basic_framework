11f4722aa26937e11cd46f5da786af60a2075e26
Diógenes Calheiros da Silva <73372540+diotech174@users.noreply.github.com>
2023-03-12 19:42:43 -0300

    tratamento de erro

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 <?php
 require_once __DIR__."/../vendor/autoload.php";
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/..");
-$dotenv->load();
+if(file_exists(__DIR__."/../.env"))
+{
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/..");
+    $dotenv->load();
+}
 
 require_once __DIR__."/../app/autoload.php";
 
